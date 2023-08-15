84b9f4dfbc5b34593b4f2aa938cdcff79087d18f
Liem Nguyen <liem1762001@gmail.com>
2023-08-15 09:51:11 +0700

    feat: edit song

--- src/scripts/services/SongService.ts
+++ src/scripts/services/SongService.ts
@@ -2,17 +2,18 @@ import ServiceBase from './ServiceBase'
 import { ISong } from '../models/SongModel'
 
 class SongService extends ServiceBase {
-    private _path = `${process.env.API_GATEWAY}/songs?_expand=genre`
+    private _path = `${process.env.API_GATEWAY}/songs`
 
     constructor() {
         super()
     }
 
-    getList = (): Promise<ISong[]> => this.get(this._path)
+    getList = (): Promise<ISong[]> => this.get(this._path + '?_expand=genre')
 
     addSong = (data: ISong): Promise<ISong> => this.post(this._path, data)
 
-    updateSong = (data: ISong): Promise<ISong> => this.patch(this._path, data)
+    updateSong = (data: ISong): Promise<ISong> =>
+        this.patch(`${this._path}/${data.id}`, data)
 }
 
 export default SongService
